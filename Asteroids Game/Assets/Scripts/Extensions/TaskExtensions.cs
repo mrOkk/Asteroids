29065da568204b9e337630f1d7a0ce8a51375220
Oleg Koposov <xtrimsmile@gmail.com>
2022-11-27 13:47:10 +0300

    Add TaskExtensions

---
+++ Asteroids Game/Assets/Scripts/Extensions/TaskExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Extensions
+{
+	public static class TaskExtensions
+	{
+		private static readonly Action<Task> _handleFinishedTask = HandleFinishedTask;
+
+		public static void Forget(this Task task)
+		{
+			task.ContinueWith(_handleFinishedTask);
+		}
+
+		private static void HandleFinishedTask(Task task)
+		{
+			if (task.IsFaulted)
+			{
+				Debug.LogException(task.Exception);
+			}
+		}
+	}
+}
