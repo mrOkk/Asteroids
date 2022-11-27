40dd9f4eef55323ead62055d0c9d854f8a3392cd
Oleg Koposov <xtrimsmile@gmail.com>
2022-11-27 14:47:01 +0300

    Add ResourceLoader services

---
+++ Asteroids Game/Assets/Scripts/Interfaces/Services/IResourceLoader.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Interfaces.Services
+{
+	public interface IResourceLoader : IService
+	{
+		Task<TResource> LoadResource<TResource>(string path) where TResource : Object;
+		void ReleaseResource(string path);
+	}
+}
