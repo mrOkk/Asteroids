da32bfbf508f0b7de5d7c732de19744ce969e94b
Oleg Koposov <xtrimsmile@gmail.com>
2022-11-27 14:18:32 +0300

    Add extension class for services

---
+++ Asteroids Game/Assets/Scripts/Extensions/ServicesExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Interfaces.Services;
+
+namespace Extensions
+{
+	public static class ServicesExtensions
+	{
+		public static async Task<TService> WaitForServiceReady<TService>(this TService instance) where TService : IService
+		{
+			await instance.InitializationTask;
+			return instance;
+		}
+	}
+}
